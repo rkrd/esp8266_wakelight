02f3e4561485268f4819b9fb7b87292a387f67ba
Rickard Gustafsson <rickard@allyourbase.se>
2017-05-27 17:59:27 +0200

    Clean up.

--- d1_rtc.ino
+++ d1_rtc.ino
@@ -29,34 +29,37 @@ const rst_info *rst_inf;
 }
 extern "C" void esp_yield();
 
-//#define NODWEET
-
+#define DEBUG
+#define MAIN_DBG
 
 #include "wifi.h"
-#define WAKEGRACE 60
-#define DSLPTIME (60 * 60)
-#ifdef D1
-#define DEBUGPIN 2
-#else
+#define DSLPTIME (15 * 60)
 #define DEBUGPIN 4 /* D2 */ /* D4 is lead on board */
-#endif
 
 #define NTP_PACKET_SIZE 48
 #define SEVENTY_YEARS 2208988800UL
 
+#define SYNC_CYCLES 1 /* Number of cycles to sleep before syncing time via NTP */
 
 
-int restore_time(void);
+void init_mem(void);
+void init_wifi(void);
+
 bool save_times(void);
+int restore_time(void);
+time_t sleep_comp(time_t expected, time_t real);
+uint32_t calc32(const uint8_t *data, size_t length);
 void set_time(String *req);
 
 void handle_client(WiFiClient *client);
 void set_alarm(String *req);
-uint32_t calc32(const uint8_t *data, size_t length);
 time_t get_ntp_time(void);
 
 void blink_debug(int);
 void dweet(String);
+
+void eeprom_save_alarm(uint8_t wday, struct alarm *a);
+void eeprom_read_alarms();
 
 // Storage capacity is 512 bytes
 struct {
@@ -79,124 +82,80 @@ byte *sleep_cycles;
 
 void setup()
 {
-    bool nosleep = false;
-    bool start_server = false;
-
+    bool init = false;
     Serial.begin(9600);
-
+    blink_debug(DEBUGPIN);
 
     Serial.print("Reset reason: ");
     rst_inf = system_get_rst_info();
 
     switch(rst_inf->reason) {
+        case REASON_DEFAULT_RST: /* Power on */
+            Serial.println("Power on");
+            init = true;
+            break;
         case REASON_DEEP_SLEEP_AWAKE:
             Serial.println("Deep-Sleep Wake");
             break;
         case REASON_EXT_SYS_RST:
-            //start_server = true;
             Serial.println("Reset");
+            init = true;
             break;
         default:
-            nosleep = true;
             Serial.println("Other");
             break;
     }
 
-    int ret = restore_time();
-    Serial.println(String("restore ret: ") + ret);
-    if(!ret) {
-        if (!rtc_data.data[0]) {
-            /* RTC data not init. */
-            *sleep_cycles = 0;
-        }
-        Serial.println("Restore time OK");
-        Serial.println(*time_now);
-        Serial.println(*time_sleep);
-        Serial.println(*time_comp);
-        for (int i = 0; i < 7; i++) {
-            Serial.print(alarms[i].a_hour);
-            Serial.print(":");
-            Serial.println(alarms[i].a_min);
-        }
+    if (init) {
+        init_mem(); 
     } else {
-        Serial.println("Restore time fail " + ret);
+        int ret = restore_time();
+        if (ret < 0) {
+            Serial.print("Something went wrong, restart ");
+            Serial.println(ret);
+            system_restart(); /* Something failed, try with reset. */
+        }
     }
 
-    blink_debug(DEBUGPIN);
-
-    /*
-    struct tm *tnow = localtime(&*time_now);
-    int now_sec = tnow->tm_hour * 3600 + tnow->tm_min * 60 + tnow->tm_sec;
-    int d = tnow->tm_wday;
-    int alarm_sec = alarms[d].a_hour * 3600 + alarms[d].a_min + alarms[d].tm_sec;
-    int diff = now_sec - alarm_sec;
-
-    if (diff < WAKEGRACE) {
-        // alarm
-    }
-    */
-
-    WiFi.begin(ssid, password);
-    Serial.println("Connecting to wifi");
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println(WiFi.localIP());
-
-    if (start_server) {
-        Serial.println("rtc timeget");
-        Serial.println(system_get_rtc_time());
-        server.begin();
-    } else {
-
-        time_t tsave = *time_now;
+    time_t tsave = *time_now;
+    if (!init && !(*sleep_cycles % SYNC_CYCLES)) {
         *time_now = get_ntp_time();
-        
-        // Check how much times differs and compensate with 2/3 of that time.
-        time_t difft = tsave - *time_now;
-        difft *= 2;
-        difft /= 3;
-        *time_comp += difft;
-        difft = *time_comp;
+        *time_comp = sleep_comp(tsave, *time_now);
+    }
+    dweet(String("restore_") + tsave + String("_ntp_") + *time_now + String("_compensation_") + *time_comp + String("_cycle_") + *sleep_cycles);
 
-        if (rst_inf->reason != REASON_DEEP_SLEEP_AWAKE) {
-            Serial.println("Reset reason not deepsleep wake, not using timediff");
-            difft = 0;
-        }
+    *time_sleep = DSLPTIME;
+    *sleep_cycles += 1;
 
-#ifndef NODWEET
-        dweet(String("restore_") + tsave + String("_ntp_") + *time_now + String("_compensation_") + difft + String("_cycle_") + *sleep_cycles);
+#ifdef MAIN_DBG
+    Serial.println("Going to sleep");
+    Serial.println(String("Time to sleep:") + *time_sleep);
+    Serial.println(String("Time nowe:") + *time_now);
+    Serial.println(String("Time compensation:") + *time_comp);
+    Serial.println(String("Sleep cycles:") + *sleep_cycles);
+    Serial.println("Go to sleep");
 #endif
 
-        // Deep sleep for configured time if time until next alarm is bigger than
-        // configured time. Otherwise sleep a bit but wake up in time for alarm.
-        *time_sleep = DSLPTIME;//diff < DSLPTIME ? diff - 10 : DSLPTIME;
-
-        save_times();
-        Serial.println("Going to sleep");
-        Serial.println(String("Time to sleep:") + *time_sleep);
-        Serial.println(String("Time nowe:") + *time_now);
-        Serial.println(String("Time compensation:") + difft);
-        Serial.println(String("Sleep cycles:") + *sleep_cycles);
-        Serial.println("Go to sleep");
-        *sleep_cycles += 1;
-        system_deep_sleep(1000000 * (*time_sleep + difft));
-        esp_yield();
-    }
+    save_times();
+    system_deep_sleep(1000000 * (*time_sleep + *time_comp));
+    esp_yield();
 }
 
 void loop()
 {
+    static bool server_started = false;
+    if (!server_started)
+        server.begin();
+
     WiFiClient client = server.available();
-    Serial.println("Wait client");
+    //Serial.println("Wait client");
     if (client) { 
         while (!client.available()) delay(1);
         Serial.println("Handle client");
         handle_client(&client);
     }
 
-    Serial.println("Client done.");
+    //Serial.println("Client done.");
 
     delay(1000);
 }
@@ -228,7 +187,7 @@ void handle_client(WiFiClient *client)
     char b[100];
     for (int i = 0; i < 7; i++) {
         snprintf(b, sizeof b, "Alarm[%d] -> %02d:%02d<br />\n",
-                i, alarms[i].a_min, alarms[i].a_min);
+                i, alarms[i].a_hour, alarms[i].a_min);
         client->print(b);
     }
 
@@ -271,6 +230,7 @@ void set_alarm(String *req)
     alarms[wday].a_hour = t / 100;
     alarms[wday].a_min = t - (t / 100) * 100;
 
+    eeprom_save_alarm(wday, alarms + wday);
     save_times();
 }
 
@@ -314,11 +274,17 @@ int restore_time(void)
     if (!system_rtc_mem_read(RTC_USRMEM_START + 0, (uint32_t*) &rtc_data, sizeof rtc_data))
         return -1;
 
-    if (!rtc_data.data[0]) {
-        uint32_t crc = calc32(((uint8_t*) &rtc_data) + 4, sizeof rtc_data - 4);
-        if (crc != rtc_data.crc32)
-            return -2;
+    uint32_t crc = calc32(((uint8_t*) &rtc_data) + 4, sizeof rtc_data - 4);
+    if (rtc_data.data[0] != 0x55 && 
+        rtc_data.data[1] != 0x55 &&
+        rtc_data.data[2] != 0x55 &&
+        rtc_data.data[3] != 0x55) {
+        rtc_data.crc32 = crc;
+        memset(rtc_data.data, 0x55, 4);
     }
+
+    if (crc != rtc_data.crc32)
+        return -2;
 
     p = rtc_data.data;
     p += MEM_ALIGN;
@@ -338,6 +304,25 @@ int restore_time(void)
     sleep_cycles = p;
 
     *time_now += *time_sleep;
+
+
+#ifdef DEBUG
+    Serial.print("rtc_data.data[0]");
+    Serial.println(rtc_data.data[0]);
+    if (!rtc_data.data[0]) {
+        /* RTC data not init. */
+        *sleep_cycles = 0;
+    }
+    Serial.println("Restore time OK");
+    Serial.println(*time_now);
+    Serial.println(*time_sleep);
+    Serial.println(*time_comp);
+    for (int i = 0; i < 7; i++) {
+        Serial.print(alarms[i].a_hour);
+        Serial.print(":");
+        Serial.println(alarms[i].a_min);
+    }
+#endif
 
     return 0;
 }
@@ -399,15 +384,20 @@ void dweet(String s)
 
 time_t get_ntp_time(void)
 {
-    WiFiUDP udp;
-    unsigned int localPort = 2390;
-    udp.begin(localPort);
-
-    IPAddress ipaddr;
     const char* ntp_url = "time.nist.gov";
-
-
+    unsigned int localPort = 2390;
+    WiFiUDP udp;
+    struct tm *ntp_tm;
+    char str[100];
     byte packet[NTP_PACKET_SIZE];
+    IPAddress ipaddr;
+    time_t t = -1;
+
+
+    if (WiFi.status() != WL_CONNECTED)
+        init_wifi();
+
+    udp.begin(localPort);
 
     WiFi.hostByName(ntp_url, ipaddr);
 
@@ -431,7 +421,7 @@ time_t get_ntp_time(void)
     udp.endPacket();
     yield();
 
-    int cb;
+    int cb, i = 0;;
     while (cb = udp.parsePacket(), !cb) {
         Serial.println("no packet yet");
         Serial.println(cb);
@@ -440,17 +430,19 @@ time_t get_ntp_time(void)
         udp.write(packet, NTP_PACKET_SIZE);
         udp.endPacket();
         yield();
+
+        if (++i == 5)
+            goto end;
     }
 
     Serial.print("packet received, length=");
     Serial.println(cb);
     udp.read(packet, NTP_PACKET_SIZE); // read the packet into the buffer
 
-    time_t t = packet[40] << 24 | packet[41] << 16 | packet[42] << 8 | packet[43];
+    t = packet[40] << 24 | packet[41] << 16 | packet[42] << 8 | packet[43];
     t -= SEVENTY_YEARS;
 
-    struct tm *ntp_tm = gmtime(&t);
-    char str[100];
+    ntp_tm = gmtime(&t);
     snprintf(str, 100, "%d-%d-%d %d:%d\n",
             ntp_tm->tm_year + 1900,
             ntp_tm->tm_mon,
@@ -460,5 +452,80 @@ time_t get_ntp_time(void)
     Serial.print(String("Time recieved: "));
     Serial.println(String(str));
 
+end:
+    udp.stop();
     return t;
+}
+
+void eeprom_save_alarm(uint8_t wday, struct alarm *a)
+{
+    uint8_t *p;
+    size_t s = sizeof *a;
+
+    EEPROM.begin(512);
+    p = EEPROM.getDataPtr();
+
+    if (!p)
+        return;
+
+    memcpy(p + (wday * s), a, s);
+    EEPROM.commit();
+    EEPROM.end();
+}
+
+void eeprom_read_alarms()
+{
+    uint8_t *p;
+    size_t s = sizeof *alarms;
+
+    EEPROM.begin(512);
+    p = EEPROM.getDataPtr();
+
+    if (!p)
+        return;
+
+    memcpy(alarms, p, 7 * s);
+
+    /* Not the cleanest exit, but end() will commit() after data ptr is received.
+       Hope it's ok ¯\_(ツ)_/¯ */
+
+#ifdef DEBUG
+    Serial.println("EEPROM saved alarms:");
+    for (int i = 0; i < 7; ++i)
+        Serial.println(String("alarm ") + alarms[i].a_hour + String(":") + alarms[i].a_min);
+#endif
+}
+
+// Get time to compensate per sleep cycle
+time_t sleep_comp(time_t expected, time_t real)
+{
+    time_t diff = expected - real;
+    diff = diff / 3;
+    diff = diff * 2;
+    diff = diff / SYNC_CYCLES; 
+
+    return diff;
+}
+
+void init_wifi(void)
+{
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting to wifi");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println(WiFi.localIP());
+}
+
+void init_mem(void)
+{
+    if (WiFi.status() != WL_CONNECTED)
+        init_wifi();
+
+    restore_time(); // Setup time pointers;
+    eeprom_read_alarms();
+    *time_comp = 0;
+    *sleep_cycles = 0;
+    *time_now = get_ntp_time();
 }
